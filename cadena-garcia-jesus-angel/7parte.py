8200a24ce84d5c339488e351f9a93901655ef095
Yaevine <Yaevine@gmail.com>
2018-04-20 15:48:30 +0200

    solo queda una

---
+++ cadena-garcia-jesus-angel/7parte.py
@@ -0,0 +1,11 @@
+#fijo las variables y le doy un valor
+list = []
+menu="si"
+#hacemos la pregunta y establecemos el valor de la lista
+palabra= input("Dime una palabra, la que quieras\n")
+while menu=="si":
+	list.append = input[palabra]
+	palabra = input("Dime otra palabra, la que quieras \n")
+	menu = input("¿Quieres saber otro numero?( si/no)\n")
+	if menu !="si":
+		print ("hola")
